d96662501be676135ee6caf155a38c95beaa4ef6
aymanshawqi360 <ayman22ahmadan2017@gmail.com>
2025-10-15 10:52:04 +0300

    added input_validator function

--- lib/functions/input_validator.dart
+++ lib/functions/input_validator.dart
@@ -1,3 +1,8 @@
 abstract class InputValidator {
-  bool isValidEmail({required String email, required RegExp emailRegex});
+  bool isValidEmail({required String email});
+  bool isValidPassword({required String password});
+  bool isValidConfirmPassword({
+    required String confirmPassword,
+    required String password,
+  });
 }
