94b6d0d14038cd100c30c132511c761572608de8
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2025-10-18 00:58:51 +0300

    finish cart

--- lib/widgets/shopping_cart.dart
+++ lib/widgets/shopping_cart.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_testing_lab/models/cart_model.dart';
 
-class CartItem {
-  final String id;
-  final String name;
-  final double price;
-  int quantity;
-  final double discount; // Discount percentage (0.0 to 1.0)
 
-  CartItem({
-    required this.id,
-    required this.name,
-    required this.price,
-    this.quantity = 1,
-    this.discount = 0.0,
-  });
-}
 
 class ShoppingCart extends StatefulWidget {
   const ShoppingCart({super.key});
